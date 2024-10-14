ce951cd10e4bb6b7169de5d982a694acc413a369
Pavel Movsesyan <movsesyanpv1995@live.com>
2024-10-14 11:39:15 +0300

    Update globals.py

--- src/pygnssutils/globals.py
+++ src/pygnssutils/globals.py
@@ -104,3 +104,72 @@ HTTPCODES = {
     404: "Not Found",
     405: "Method Not Allowed",
 }
+
+# map of fix values to descriptions
+# the keys in this map are a concatenation of NMEA/UBX
+# message identifier and attribute value e.g.:
+# GGA1: GGA + quality = 1
+# NAV-STATUS3: NAV-STATUS + gpsFix = 3
+# (valid for NMEA >=4)
+FIXLOOKUP = {
+    "GGA1": "3D",  # quality
+    "GGA2": "3D",
+    "GGA4": "RTK FIXED",
+    "GGA5": "RTK FLOAT",
+    "GGA6": "DR",
+    "GLLA": "3D",  # posMode
+    "GLLD": "RTK",
+    "GLLE": "DR",
+    "GNSA": "3D",  # posMode
+    "GNSD": "3D",
+    "GNSF": "RTK FLOAT",
+    "GNSR": "RTK FIXED",
+    "GNSE": "DR",
+    "RMCA": "3D",  # posMode
+    "RMCD": "3D",
+    "RMCF": "RTK FLOAT",
+    "RMCR": "RTK FIXED",
+    "RMCE": "DR",
+    "VTGA": "3D",  # posMode
+    "VTGD": "RTK",
+    "VTGE": "DR",
+    "NAV-PVT1": "DR",  # fixType
+    "NAV-PVT2": "2D",
+    "NAV-PVT3": "3D",
+    "NAV-PVT4": "GNSS+DR",
+    "NAV-PVT5": "TIME ONLY",
+    "NAV-PVT6": "RTK FLOAT",  # carrSoln
+    "NAV-PVT7": "RTK FIXED",
+    "NAV-STATUS1": "DR",  # gpsFix
+    "NAV-STATUS2": "3D",
+    "NAV-STATUS3": "3D",
+    "NAV-STATUS4": "GNSS+DR",
+    "NAV-STATUS5": "TIME ONLY",
+    "NAV-STATUS6": "RTK FLOAT",  # carrSoln
+    "NAV-STATUS7": "RTK FIXED",
+    "NAV-SOL1": "DR",  # gpsFix
+    "NAV-SOL2": "3D",
+    "NAV-SOL3": "3D",
+    "NAV-SOL4": "GNSS+DR",
+    "NAV-SOL5": "TIME ONLY",
+    "HNR-PVT1": "DR",  # gpsFix
+    "HNR-PVT2": "2D",
+    "HNR-PVT3": "3D",
+    "HNR-PVT4": "GNSS+DR",
+    "HNR-PVT5": "TIME ONLY",
+    "HNR-PVT6": "RTK",
+    "NAV2-PVT1": "DR",  # fixType
+    "NAV2-PVT2": "2D",
+    "NAV2-PVT3": "3D",
+    "NAV2-PVT4": "GNSS+DR",
+    "NAV2-PVT5": "TIME ONLY",
+    "NAV2-PVT6": "RTK FLOAT",  # carrSoln
+    "NAV2-PVT7": "RTK FIXED",
+    "NAV2-STATUS1": "DR",  # gpsFix
+    "NAV2-STATUS2": "3D",
+    "NAV2-STATUS3": "3D",
+    "NAV2-STATUS4": "GNSS+DR",
+    "NAV2-STATUS5": "TIME ONLY",
+    "NAV2-STATUS6": "RTK FLOAT",  # carrSoln
+    "NAV2-STATUS7": "RTK FIXED",
+}
